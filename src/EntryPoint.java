91a59f875b2cb57d6d28c1d0a84aae7145cbeb7b
Fábio Monsores <fabio2009pbi@gmail.com>
2023-07-19 12:37:13 -0300

    tarefa finalizada

---
+++ src/EntryPoint.java
@@ -0,0 +1,14 @@
+//Importe sua classe aqui
+
+class EntryPoint {
+    public static void main(String[] args) {
+        //Instancie sua classe aqui  
+         Pessoa pessoa1 = new Pessoa();
+
+    pessoa1.primeiroNome("Fábio");
+    pessoa1.sobrenome("Monsores");
+    pessoa1.idade(31);
+    pessoa1.sexo("M");
+    pessoa1.imprimirPessoa();      
+    }
+}
